cd411e850e8df52be97b5b4fedaf0d4ed2d16ad1
Ashish <akhatkar64@gmail.com>
2014-11-20 15:42:31 +0530

    Java Examples

---
+++ Java_Examples/src/Version1.java
@@ -0,0 +1,25 @@
+/*
+ * @author: Ashish Khatkar, Pulkit Manocha, Anindya Srivastava, Masood Pathan
+ */
+/*
+ * This is version 1
+ */
+import java.io.*;
+public class Version1 
+{
+	public static void func(int a, int b)
+	{
+		if(a<b)
+			System.out.println(b/a);
+		else
+			System.out.println(a/b);
+	}
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String str[]=br.readLine().split(" ");
+		int a=Integer.parseInt(str[0]);
+		int b=Integer.parseInt(str[1]);
+		func(a,b);
+	}
+}
---
+++ Java_Examples/src/Version3.java
@@ -0,0 +1,29 @@
+/*
+ * @author: Ashish Khatkar, Pulkit Manocha, Anindya Srivastava, Masood Pathan
+ */
+/*
+ * This is version 1
+ */
+import java.io.*;
+public class Version3
+{
+	public static void func(int a, int b)
+	{
+		if(a==0)
+			System.out.println(a/b);
+		else if(b==0)
+			System.out.println(b/a);
+		else if(a<b)
+			System.out.println(b/a);
+		else
+			System.out.println(a/b);
+	}
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String str[]=br.readLine().split(" ");
+		int a=Integer.parseInt(str[0]);
+		int b=Integer.parseInt(str[1]);
+		func(a,b);
+	}
+}
